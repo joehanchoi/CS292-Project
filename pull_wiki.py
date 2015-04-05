67c38fe61a9fce59c126b65bc938277893c76a44
Linda Zhang <zhanl10@hadoop-node4.vampire>
2015-04-05 01:38:50 -0500

    Code for creating tables & pulling from wikipedia.

--- pull_wiki.py
+++ pull_wiki.py
@@ -1,5 +1,6 @@
 from DB import DB, DB_NAME
 import wikipedia
+import re
 
 def create_table(db):
 	con = db.connection()
@@ -12,17 +13,53 @@ def create_table(db):
 		cur.execute(q)
 	con.commit()
 
-def load_terms():
-	pass
+def load_wiki(db):
+	med_terms = []
+	with open('snomed_cleaned_term.txt','rb') as f:
+		text = f.readlines()
+	for line in text:
+		med_terms.extend(line.split(','))
 
-def load_wiki():
-	test = wikipedia.page("Cancer")
-	print test.title
+	con = db.connection()
+	cur = db.cursor()
+	missed = 0
+
+	for term in med_terms:
+		try:
+			page = wikipedia.page(term)
+		except wikipedia.exceptions.DisambiguationError as e:
+			e.options = [t.encode('utf-8') for t in e.options]
+			possible = [x for x in e.options if re.search('medic', x.lower())]
+			if possible:
+				try:
+					page = wikipedia.page(possible[0])
+				except:
+					missed += 1
+					continue
+			else:
+				try:
+					page = wikipedia.page(e.options[0])
+				except:
+					missed += 1
+					continue
+			#print page.title
+		except wikipedia.exceptions.PageError:
+			missed += 1
+			continue
+
+		try:
+			categories = ",".join(page.categories)
+		except:
+			categories = ""
+
+		cur.execute("insert into med_pages VALUES (%s, %s, %s, %s, %s)",(int(page.pageid),page.title,page.summary,categories,page.content))
+	con.commit()
+	print '# unidentifiable pages:', missed
 
 def main():
 	db = DB(DB_NAME)
-	load_terms()
-	load_wiki()
+	create_table(db)
+	load_wiki(db)
 
 if __name__=='__main__':
 	main()
