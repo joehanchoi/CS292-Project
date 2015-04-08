14ce7e91cb15de500fad3ae8a6418a282187993e
Linda Zhang <zhanl10@hadoop-node4.vampire>
2015-04-08 16:38:18 -0500

    Added a couple touches to visualization.

--- visualize.py
+++ visualize.py
@@ -11,13 +11,16 @@ def main():
 	graph(test_term)
 
 def add_x_nodes(pages, G, id, i):
+	nodes = []
 	title = pages.get_title(int(id))
 	sim_pages = pages.query_page(id)
-	short_sim_pages = sorted(sim_pages, key=lambda x: x[1], reverse=True)[1:i]
+	short_sim_pages = sorted(sim_pages, key=lambda x: x[1], reverse=True)[1:i+1]
 	for page in short_sim_pages:
 		page_name = pages.get_title(int(page[0]))
 		G.add_node(page_name)
+		nodes.append(page_name)
 		G.add_edge(title,page_name)
+	return nodes
 
 def graph(term):
 	pages = Pages()
@@ -25,17 +28,33 @@ def graph(term):
 	sim_pages = pages.query_page(id)
 
 	G = nx.Graph()
+	node_dict = {}
 	title = pages.get_title(int(id))
 	G.add_node(title)
+	node_dict[title] = ('red', 0.0)
 
 	short_sim_pages = sorted(sim_pages, key=lambda x: x[1], reverse=True)[1:11]
 	for page in short_sim_pages:
 		page_name = pages.get_title(int(page[0]))
 		G.add_node(page_name)
+		node_dict[page_name] = ('blue', float(page[1]))
 		G.add_edge(title,page_name)
-		add_x_nodes(pages, G, page[0], 10)
+		addl_nodes = add_x_nodes(pages, G, page[0], 10)
+		for node in addl_nodes:
+			if node not in node_dict:
+				node_dict[node] = ('green', float(page[1]))
 
-	nx.draw(G)
+	node_list = []
+	node_color = []
+	node_size = []
+	for node in node_dict:
+		node_list.append(node)
+		node_color.append(node_dict[node][0])
+		node_size.append(node_dict[node][1])
+	node_size = [x*300/max(node_size) for x in node_size]
+	node_size[node_size.index(0.0)]=300
+
+	nx.draw_networkx(G=G,with_labels=True,nodelist=node_list,node_size=node_size,node_color=node_color,font_size=8)
 	plt.savefig("test.png")
 
 if __name__=='__main__':
