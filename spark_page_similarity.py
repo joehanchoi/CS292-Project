bad5654128c0f257019e8af016639560f9d3eb94
Linda Zhang <zhanl10@hadoop-node4.vampire>
2015-04-28 18:25:59 -0500

    Update spark page similarity.

--- spark_page_similarity.py
+++ spark_page_similarity.py
@@ -10,12 +10,12 @@ class Pages:
 	def __init__(self):
 		self.hbase = Hbase(DB_NAME)
 		self.create_similarity_hbase_table()
-		self.table = self.hbase.table("spark_pages_similarity")
+		self.table = self.hbase.table("spark_pages_similarity_umls_subset")
 
 	def create_similarity_hbase_table(self):
 		self.hbase_connection = self.hbase.get_connection()
 		try:
-			self.hbase_connection.create_table("spark_pages_similarity", {'paired_page':dict()})
+			self.hbase_connection.create_table("spark_pages_similarity_umls_subset", {'paired_page':dict()})
 		except happybase.hbase.ttypes.AlreadyExists:
 			pass
 
@@ -24,17 +24,34 @@ class Pages:
 
 def spark_similarity(data, stop_words):
 	spark_pages = Pages()
-	conf = SparkConf().setAppName('J_Similarity').setMaster('local[4]')
+	conf = SparkConf().setAppName('J_Similarity').setMaster('local[4]')#.set("spark.cores.max", "1")
 	sc = SparkContext(conf=conf)
 	input = [(pid, set(re.findall(r"[\w']+", data[pid].lower())).difference(stop_words)) for pid in data.keys() if len(set(re.findall(r"[\w']+", data[pid].lower())).difference(stop_words))>0]
-	result = sc.parallelize(input) \
+	result = sc.parallelize(temp_input) \
 		.keyBy(lambda x:1)
 	result = result.join(result) \
 		.map(lambda x:x[1]) \
 		.map(lambda x:((x[0][0], x[1][0]), (1.0 * len(x[0][1].intersection(x[1][1]))) / len(x[0][1].union(x[1][1]))))
 	result = result.collect()
 	for pid, sim in result:
-		spark_pages.insert(pid[0],pid[1],sim)
+		if sim > 0:
+			spark_pages.insert(pid[0],pid[1],sim)
+
+def read_sql():
+	with open('stop_words.txt', 'rb') as f:
+		stop_list = f.readlines()
+	stop_words = set()
+	for line in stop_list:
+		stop_words.update(line.split(','))
+
+	data = {}
+	db = DB(DB_NAME)
+
+	result = db.query("select page_id, summary from med_pages_umls_subset")
+	for row in result:
+		data[row[0]] = row[1]
+
+	return data, stop_words
 
 def read_json(file_name):
 	with open('stop_words.txt', 'rb') as f:
@@ -51,7 +68,8 @@ def read_json(file_name):
 	return data, stop_words
 
 def main():
-	data, stop_words = read_json('pages.json')
+	#data, stop_words = read_json('pages.json')
+	data, stop_words = read_sql()
 	spark_similarity(data, stop_words)
 
 if __name__=='__main__':
