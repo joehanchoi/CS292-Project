5681ea08ad9541dbd41fe1efe9cc099ce7a8549b
Linda Zhang <zhanl10@hadoop-node4.vampire>
2015-04-27 22:22:28 -0500

    Spark implementation for page similarity.

---
+++ spark_page_similarity.py
@@ -0,0 +1,58 @@
+import pyspark
+from pyspark import SparkContext, SparkConf
+import numpy as np
+import happybase
+import json
+import re
+from DB import DB, DB_NAME, Hbase
+
+class Pages:
+	def __init__(self):
+		self.hbase = Hbase(DB_NAME)
+		self.create_similarity_hbase_table()
+		self.table = self.hbase.table("spark_pages_similarity")
+
+	def create_similarity_hbase_table(self):
+		self.hbase_connection = self.hbase.get_connection()
+		try:
+			self.hbase_connection.create_table("spark_pages_similarity", {'paired_page':dict()})
+		except happybase.hbase.ttypes.AlreadyExists:
+			pass
+
+	def insert(self, pid1, pid2, sim):
+		self.table.put(str(pid1), {'paired_page:'+str(pid2):str(sim)})
+
+def spark_similarity(data, stop_words):
+	spark_pages = Pages()
+	conf = SparkConf().setAppName('J_Similarity').setMaster('local[4]')
+	sc = SparkContext(conf=conf)
+	input = [(pid, set(re.findall(r"[\w']+", data[pid].lower())).difference(stop_words)) for pid in data.keys() if len(set(re.findall(r"[\w']+", data[pid].lower())).difference(stop_words))>0]
+	result = sc.parallelize(input) \
+		.keyBy(lambda x:1)
+	result = result.join(result) \
+		.map(lambda x:x[1]) \
+		.map(lambda x:((x[0][0], x[1][0]), (1.0 * len(x[0][1].intersection(x[1][1]))) / len(x[0][1].union(x[1][1]))))
+	result = result.collect()
+	for pid, sim in result:
+		spark_pages.insert(pid[0],pid[1],sim)
+
+def read_json(file_name):
+	with open('stop_words.txt', 'rb') as f:
+		stop_list = f.readlines()
+	stop_words = set()
+	for line in stop_list:
+		stop_words.update(line.split(','))
+
+	data={}
+	with open(file_name, 'rb') as data_file:
+	    for line in data_file:
+			d = dict(json.loads(line))
+			data[d['page_id']]=d['summary']
+	return data, stop_words
+
+def main():
+	data, stop_words = read_json('pages.json')
+	spark_similarity(data, stop_words)
+
+if __name__=='__main__':
+	main()
